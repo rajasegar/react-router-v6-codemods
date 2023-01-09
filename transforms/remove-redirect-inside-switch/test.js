d1439194d8c29f410bea67f7187a5120aa4741ce
Rajasegar <rajasegar.c@gmail.com>
2023-01-09 14:52:08 +0530

    chore: fix lint errors

--- transforms/remove-redirect-inside-switch/test.js
+++ transforms/remove-redirect-inside-switch/test.js
@@ -2,7 +2,7 @@
 
 const { runTransformTest } = require('codemod-cli');
 
-runTransformTest({ 
+runTransformTest({
   name: 'remove-redirect-inside-switch',
   path: require.resolve('./index.js'),
   fixtureDir: `${__dirname}/__testfixtures__/`,
